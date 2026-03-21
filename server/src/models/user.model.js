d2a8c0e62ca6ad2728d2b2629afdf9ef1cc47e6b
Code Source <codesource2001@gmail.com>
2026-03-22 03:43:03 +0530

    user.model.js file updated

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true, min: 3, max: 30 },
+    lastName: { type: String, required: true, min: 3, max: 30 },
+    username: { type: String, required: true, unique: true, min: 6, max: 30 },
+    phoneNumber: { type: String, required: true, min: 10, max: 10 },
     email: {
         type: String,
         required: true,
@@ -8,8 +12,16 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
-    }
+        required: true,
+        min: 8,
+        max: 100
+    },
+    avatar: String,
+    status: { type: String, required: true, default: 'active' },
+    lastLogin: Date,
+    role: { type: String, required: true, default: "user" },
+    permissions: { type: [String] },
+    isEmailVerified: { type: Boolean, default: false },
 }, {
     timestamps: true
 });
